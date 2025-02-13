4b83205075ef226dd47415e9448d43c705a17bf2
maksimmerkulov <merkulov.mv@outlook.com>
2025-02-13 12:02:53 +0500

    1. Модульные тесты [#505277]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,12 +3,10 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        // Assuming the conversion rate is 1 Euro = 98.10 Rubles.
         return value / 98.10f;
     }
 
     public static float rubleToDollar(float value) {
-        // Assuming the conversion rate is 1 Dollar = 94 Rubles.
         return value / 94;
     }
 }

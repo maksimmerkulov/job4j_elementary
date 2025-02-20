fe0c8f186b8a0805d7490ceb6aa4bb369fcc877a
maksimmerkulov <merkulov.mv@outlook.com>
2025-02-20 15:14:36 +0500

    Простое число (оператор прерывания break) [#505214]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        if (number < 2) {
+            return false;
+        }
+        boolean result = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}

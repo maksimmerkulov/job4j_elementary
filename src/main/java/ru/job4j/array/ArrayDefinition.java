de924bdb76342e4c972e000708a6bfa7243e667f
maksimmerkulov <merkulov.mv@outlook.com>
2025-02-22 11:48:17 +0500

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,8 +7,19 @@ public class ArrayDefinition {
         String[] surnames = new String[100500];
         float[] prices = new float[40];
 
+        String[] names = new String[4];
+        names[0] = "Maksim Merkulov";
+        names[1] = "James Gosling";
+        names[2] = "Josh Bloch";
+        names[3] = "Herbert Schildt";
+
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        System.out.println();
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }

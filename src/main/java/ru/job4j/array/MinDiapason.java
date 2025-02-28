a69b194f7ccd64882ad979ba63cf6892dba1eb6d
maksimmerkulov <merkulov.mv@outlook.com>
2025-02-28 08:15:24 +0500

    Поиск минимума в диапазоне [#505239 #534130]

---
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * Класс MinDiapason предназначен для поиска минимального значения
+ * в заданном диапазоне индексов массива.
+ */
+public class MinDiapason {
+
+    /**
+     * Метод находит минимальное значение в массиве в пределах указанного диапазона.
+     *
+     * @param array  Входной массив.
+     * @param start  Начальный индекс диапазона (включительно).
+     * @param finish Конечный индекс диапазона (включительно).
+     * @return Минимальное значение в указанном диапазоне.
+     */
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int i = start + 1; i <= finish; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}

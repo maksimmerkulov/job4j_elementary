63036e58d1dfaf9c03f37c09932921dec5072e1a
maksimmerkulov <merkulov.mv@outlook.com>
2025-04-05 19:54:12 +0500

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,11 +4,14 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
 /**
- * Класс {@code Point} предназначен для представления точки на двумерной плоскости
+ * Класс {@code Point} предназначен для представления точки на двумерной или трехмерной плоскости
  * и вычисления расстояния до другой точки по формуле Евклидова расстояния.
  * <p>
- * Формула расчета расстояния между двумя точками (x1, y1) и (x2, y2):
+ * Для 2D используется формула:
  * {@code sqrt((x2 - x1)^2 + (y2 - y1)^2)}
+ * <p>
+ * Для 3D:
+ * {@code sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2)}
  * </p>
  *
  * <p>Пример использования:</p>
@@ -19,8 +22,8 @@ import static java.lang.Math.pow;
  * </pre>
  *
  * @author Maksim Merkulov
- * @version 1.2
- * @since 2025-04-03
+ * @version 1.3
+ * @since 2025-04-05
  */
 public class Point {
 
@@ -37,30 +40,83 @@ public class Point {
     private int y;
 
     /**
-     * Конструктор {@code Point(int first, int second)} создает объект {@link Point} с заданными координатами.
-     *
-     * @param first  Координата X.
-     * @param second Координата Y.
+     * Поле {@code z} представляет координату Z точки.
+     * Доступно только внутри экземпляра класса.
      */
-    public Point(int first, int second) {
-        this.x = first;
-        this.y = second;
+    private int z;
+
+    /**
+     * Конструктор {@code Point(int x, int y)}
+     * создает объект {@link Point} с заданными координатами.
+     *
+     * @param x Координата X.
+     * @param y Координата Y.
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     /**
-     * Метод {@code distance(Point that)} вычисляет Евклидово расстояние между текущей точкой и переданной в аргументе.
+     * Конструктор {@code Point(int x, int y, int z)}
+     * создает объект {@link Point} с заданными координатами.
+     *
+     * @param x Координата X.
+     * @param y Координата Y.
+     * @param z Координата Z.
+     */
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Метод {@code distance(Point that)} вычисляет Евклидово расстояние
+     * между текущей точкой и переданной в аргументе в двухмерном пространстве.
      *
      * @param that Другая точка, до которой вычисляется расстояние.
-     * @return Расстояние между двумя точками.
+     * @return Расстояние между двумя точками в 2D.
      */
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     /**
-     * Метод {@code main(String[] args)} демонстрирует работу метода {@link #distance(Point)}.
+     * Метод {@code distance3d(Point that)} вычисляет Евклидово расстояние
+     * между текущей точкой и переданной в аргументе в трехмерном пространстве.
+     *
+     * @param that Другая точка, до которой вычисляется расстояние.
+     * @return Расстояние между двумя точками в 3D.
+     */
+    public double distance3d(Point that) {
+        return sqrt(
+                pow(this.x - that.x, 2)
+                        + pow(this.y - that.y, 2)
+                        + pow(this.z - that.z, 2)
+        );
+    }
+
+    /**
+     * Метод {@code infoPoint2d()} выводит координаты точки в 2D в консоль в формате: Point[x, y].
+     */
+    public void infoPoint2d() {
+            System.out.println("Point[" + this.x + ", " + this.y + "]");
+    }
+
+    /**
+     * Метод {@code infoPoint3d()} выводит координаты точки в 3D в консоль в формате: Point[x, y, z].
+     */
+    public void infoPoint3d() {
+            System.out.println("Point[" + this.x + ", " + this.y + ", " + this.z + "]");
+    }
+
+    /**
+     * Метод {@code main(String[] args)} демонстрирует использование
+     * методов {@link #distance(Point)} и {@link #distance3d(Point)}.
      * <p>
-     * Создаются две точки и вычисляется расстояние между ними.
+     * Создаются две точки в двумерном пространстве и две точки в трехмерном пространстве.
+     * Вычисляется расстояние между точками в 2D и 3D, а затем выводятся результаты в консоль.
      * </p>
      *
      * @param args Аргументы командной строки (не используются).
@@ -68,7 +124,15 @@ public class Point {
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
+        Point c = new Point(1, 2, 3);
+        Point d = new Point(4, 5, 6);
+        a.infoPoint2d();
+        b.infoPoint2d();
+        c.infoPoint3d();
+        d.infoPoint3d();
         double distance = a.distance(b);
+        double distance3d = c.distance3d(d);
         System.out.println(distance);
+        System.out.println(distance3d);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -4,16 +4,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 /**
- * Тест-класс {@code PointTest} предназначен для тестирования метода {@link Point#distance(Point)}
- * класса {@link Point}, который вычисляет Евклидово расстояние между точками.
+ * Тест-класс {@code PointTest} предназначен для тестирования методов класса {@link Point}.
+ * В частности, для проверки метода {@link Point#distance(Point)} для вычисления Евклидова расстояния
+ * между двумя точками и метода {@link Point#distance3d(Point)} для вычисления расстояния между
+ * точками в трехмерном пространстве.
  * <p>
- * Для тестирования используются точки с различными координатами,
- * а результаты сравниваются с ожидаемыми значениями.
+ * Для тестирования используются точки с различными координатами, а результаты сравниваются с ожидаемыми значениями.
  * </p>
  *
  * @author Maksim Merkulov
- * @version 1.2
- * @since 2025-04-03
+ * @version 1.3
+ * @since 2025-04-05
  */
 class PointTest {
 
@@ -24,7 +25,7 @@ class PointTest {
     @Test
     void whenPoints00And20Then2() {
         Point a = new Point(0, 0);
-        Point b = new Point(2, 0);
+        Point b = new Point(0, 2);
         double expected = 2.0;
         double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
@@ -53,6 +54,71 @@ class PointTest {
         Point b = new Point(2, 2);
         double expected = 5.66;
         double output = a.distance(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    /**
+     * Тест проверяет расстояние между точками (0, 0) и (0, 0).
+     * Ожидаемое значение: 0.0.
+     */
+    @Test
+    void whenPoints00And00Then0() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        double expected = 0.0;
+        double output = a.distance(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    /**
+     * Тест проверяет расстояние между точками с отрицательными координатами (-3, -3) и (-6, -7).
+     * Ожидаемое значение: 5.0.
+     */
+    @Test
+    void whenPointsMinus3Minus3AndMinus6Minus7Then5() {
+        Point a = new Point(-3, -3);
+        Point b = new Point(-6, -7);
+        double expected = 5.0;
+        double output = a.distance(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    /**
+     * Тест проверяет расстояние между точками (1, 2, 3) и (4, 5, 6).
+     * Ожидаемое значение: 5.2.
+     */
+    @Test
+    void whenPoints123And456Then5Dot2() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(4, 5, 6);
+        double expected = 5.2;
+        double output = a.distance3d(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    /**
+     * Тест проверяет расстояние между точками (1, 1, 1) и (1, 1, 1).
+     * Ожидаемое значение: 0.0.
+     */
+    @Test
+    void whenPoints111And111Then0() {
+        Point a = new Point(1, 1, 1);
+        Point b = new Point(1, 1, 1);
+        double expected = 0.0;
+        double output = a.distance3d(b);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    /**
+     * Тест проверяет расстояние между точками (0, 0, 0) и (3, 4, 12).
+     * Ожидаемое значение: 13.0.
+     */
+    @Test
+    void whenPoints000And3412Then13() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(3, 4, 12);
+        double expected = 13.0;
+        double output = a.distance3d(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 }

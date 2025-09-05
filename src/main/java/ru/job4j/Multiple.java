89dd1e8237dde6023ebaab231c3ac2a99e1b2199
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-05 15:30:00 +0500

    Analyzing the first program "professionally" [#505151 #532313]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,34 @@
+package ru.job4j;
+
+/**
+ * Multiplication table for the number one.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * 1 * 2 = 2
+ * 1 * 3 = 3
+ * ...
+ * 1 * 9 = 9
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Multiple {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
+        System.out.println("1 * 9 = 9");
+    }
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+/**
+ * Student information display class.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * Maksim Merkulov
+ * 03.08.1987
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class StudentInfo {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        System.out.println("Maksim Merkulov");
+        System.out.println("03.08.1987");
+    }
+}

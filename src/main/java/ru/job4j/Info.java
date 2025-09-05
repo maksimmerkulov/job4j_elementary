0986e1e541d550de9e2d44121f5cbd3633d4d94a
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-05 16:00:00 +0500

    Header in Java file [#505272 #532315]

---
+++ src/main/java/ru/job4j/Info.java
@@ -0,0 +1,24 @@
+package ru.job4j;
+
+/**
+ * Information display class.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * 05.09.2025
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Info {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        System.out.println("05.09.2025");
+    }
+}

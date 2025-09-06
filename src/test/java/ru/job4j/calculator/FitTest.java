e74567e973e414a7982b3fe9f07491fdd20c04e8
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-06 16:00:00 +0500

    Tests for the "Ideal Weight" Task [#505260 #532577]

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+/**
+ * Unit tests for the {@link Fit} class.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+class FitTest {
+
+    /**
+     * Verifies the ideal man weight calculation.
+     */
+    @Test
+    void whenMan180Then92() {
+        short input = 180;
+        double expected = 92;
+        double output = Fit.manWeight(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    /**
+     * Verifies the ideal woman weight calculation.
+     */
+    @Test
+    void whenWoman170Then69() {
+        short input = 170;
+        double expected = 69;
+        double output = Fit.womanWeight(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}

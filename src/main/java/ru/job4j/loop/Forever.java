48d5a1c77213191ae5a4c3cc1aa75c1f688df222
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-08 15:30:00 +0500

    Infinite For Loop [#505209 #533253]

---
+++ src/main/java/ru/job4j/loop/Forever.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+/**
+ * An entry point program that displays the string "Forever" to the console
+ * ten times.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * Forever
+ * Forever
+ * ...
+ * Forever
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Forever {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Forever");
+        }
+    }
+}

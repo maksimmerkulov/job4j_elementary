af0a36d231cef140cc786db0b5b0d6e94abb13a9
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-08 14:30:00 +0500

    For loop. Definition [#505207 #533055]

---
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+/**
+ * Iterates through a range of numbers and prints them to the console.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * 10
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class LoopFor {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        for (int i = 5; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}

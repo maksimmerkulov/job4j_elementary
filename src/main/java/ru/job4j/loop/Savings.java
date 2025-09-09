150c48684257941cc8823636a00c1c0d4db36f7f
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-09 16:00:00 +0500

    Do-While Loop [#505216 #533385]

---
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+/**
+ * A utility class providing a method for calculating savings accumulation
+ * over time.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Savings {
+
+    /**
+     * Calculates the number of years to reach the {@code goal}.
+     *
+     * @param goal          the target sum
+     * @param annualDeposit the annual replenishment amount
+     * @param percent       the annual interest rate
+     * @return the number of years
+     */
+    public static int years(double goal, double annualDeposit, double percent) {
+        int years = 0;
+        double total = 0;
+        do {
+            total = total + annualDeposit + (total * percent / 100);
+            years++;
+        } while (total < goal);
+        return years;
+    }
+}

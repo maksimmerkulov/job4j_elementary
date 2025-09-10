060b3a285d8084fdb786a387649283c10fbcdc78
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-10 14:30:00 +0500

    Array declaration [#505220 #533484]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ * Defines various arrays to demonstrate declaration.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class ArrayDefinition {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}

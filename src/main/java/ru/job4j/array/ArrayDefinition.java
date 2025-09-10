b22cf40c694bf929a498ae345f964ba170cfb77c
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-10 15:00:00 +0500

    Array size [#505221 #533487]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,10 +1,17 @@
 package ru.job4j.array;
 
 /**
- * Defines various arrays to demonstrate declaration.
+ * Defines various arrays and prints their lengths to the console.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * The length of the ages array is: 10
+ * The length of the surnames array is: 100500
+ * The length of the prices array is: 40
+ * }</pre>
  *
  * @author Maksim Merkulov
- * @version 1.0
+ * @version 1.1
  */
 public class ArrayDefinition {
 
@@ -17,5 +24,11 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("The length of the ages array is: "
+                + ages.length);
+        System.out.println("The length of the surnames array is: "
+                + surnames.length);
+        System.out.println("The length of the prices array is: "
+                + prices.length);
     }
 }

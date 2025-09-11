9cfa10b10bc3126738437a6842bc8377bbb120fc
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-11 16:30:00 +0500

    Sorting an Array [#505228 #533723]

---
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+/**
+ * Sorts an array using basic swap operations.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class AlgoArray {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}

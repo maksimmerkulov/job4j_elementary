6d4f3d613d47c8c3b26acb37cb39060f519e2a8e
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-15 16:00:00 +0500

    Selection Sort [#505240 #534133]

---
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for the {@link SortSelected} class.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+class SortSelectedTest {
+
+    /**
+     * Verifies the sorting of an array with five elements.
+     */
+    @Test
+    void whenSort() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    /**
+     * Verifies the sorting of an array with the three elements.
+     */
+    @Test
+    void whenSortThreeElements() {
+        int[] data = new int[] {3, 1, 2};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3};
+        assertThat(result).containsExactly(expected);
+    }
+
+    /**
+     * Verifies the sorting of another array with the five elements.
+     */
+    @Test
+    void whenSortFiveElements() {
+        int[] data = new int[] {5, 3, 4, 1, 2};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
+}

e5f59d73427e8208a94f5f44265c3cbc2db7e67e
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-15 15:30:00 +0500

    Finding a Minimum in a Range [#505239 #534130]

---
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+/**
+ * A utility class that provides a method to find the minimum value within
+ * a range.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class MinDiapason {
+
+    /**
+     * Searches for the minimum element from the {@code start} to
+     * the {@code finish} index.
+     *
+     * @param array  the array to search in
+     * @param start  the start index (inclusive)
+     * @param finish the end index (inclusive)
+     * @return the minimum value found in the range
+     */
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int i = start + 1; i <= finish; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}

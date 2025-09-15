6bcf5cd593ae1381f3de770748442a59bf55bb95
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-15 14:30:00 +0500

    Finding an Index in a Range [#505237 #534121]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,10 +1,10 @@
 package ru.job4j.array;
 
 /**
- * A utility class providing a method for searching elements in an array.
+ * A utility class providing methods for searching elements in integer arrays.
  *
  * @author Maksim Merkulov
- * @version 1.0
+ * @version 1.1
  */
 public class FindLoop {
 
@@ -18,6 +18,31 @@ public class FindLoop {
     public static int indexOf(int[] data, int element) {
         int result = -1;
         for (int i = 0; i < data.length; i++) {
+            if (data[i] == element) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Searches for the element within the specified range.
+     *
+     * @param data    the array to search
+     * @param element the value to find
+     * @param start   the start index (inclusive)
+     * @param finish  the end index (inclusive)
+     * @return the index of the element if found, otherwise {@code -1}
+     */
+    public static int indexInRange(
+            int[] data,
+            int element,
+            int start,
+            int finish
+    ) {
+        int result = -1;
+        for (int i = start; i <= finish; i++) {
             if (data[i] == element) {
                 result = i;
                 break;

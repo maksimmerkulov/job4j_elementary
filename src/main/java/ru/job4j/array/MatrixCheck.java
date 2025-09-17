ff562ab3f671d6ee83a1d24bb21fcdf69a10a087
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-17 14:30:00 +0500

    Mono Row in a Matrix [#505245 #534374]

---
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+/**
+ * A utility class that provides a method to check matrices for specific
+ * patterns.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class MatrixCheck {
+
+    /**
+     * Checks if the specified row is fully filled with {@code 'X'}.
+     *
+     * @param board the matrix to check
+     * @param row   the index of the row to verify
+     * @return {@code true} if the row is mono, otherwise {@code false}
+     */
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}

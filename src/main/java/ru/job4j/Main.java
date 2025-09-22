41b3e3678ce6eb8e2cc8861108aac64b03d35196
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-22 14:30:00 +0500

    Code reading and basic errors [#40205 #537397]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,6 +1,23 @@
 package ru.job4j;
 
+/**
+ * Starts the application and prints "Hello, world!" to the console.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * Hello, world!
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
 public class Main {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
